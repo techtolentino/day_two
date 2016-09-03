19e8f98195dacdea18b6cd631954674001a193b7
Randy Tolentino <makertolentino@gmail.com>
2016-09-03 09:15:51 -0500

    some styling - will need refactoring

--- routes/users.js
+++ routes/users.js
@@ -61,25 +61,44 @@ passport.use(new LocalStrategy(
 	function(username, password, done) {
 		User.getUserByUsername(username, function(err, user) {
 			if(err) throw err;
+			
 			if(!user){
 				return done(null, false, {message: 'Unknown user'});
 			}
+			
 			User.comparePassword(password, user.password, function(err, isMatch){
 				if(err) throw err;
 				if(isMatch) {
 					return done(null, user);
 				} else {
-					return done(null, false, {message: ''})
+					return done(null, false, {message: 'Invalid password'})
 				}
 			});
 		});
 	}
 ));
 
-router.post('/login',
-	passport.authenticate('local', {successRedirect: '/', failureRedirect: '/users/login', failureFlash: true}),
-	function(req, res) {
-		res.redirect('/');
+passport.serializeUser(function(user, done) {
+	done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+	User.getUserById(id, function(err, user) {
+		done(err, user);
 	});
+});
+
+router.post('/login', passport.authenticate('local',
+		{successRedirect: '/', failureRedirect: '/users/login', failureFlash: true}),
+	function(req, res) {
+		res.redirect('/starter');
+});
+
+router.get('/logout', function(req, res) {
+	req.logout();
+	req.flash('success_msg', 'You are logged out');
+	res.redirect('/users/login');
+})
+
 
 module.exports = router;
