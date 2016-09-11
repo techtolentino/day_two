d52fc04a030f88b1e84a18c7346d01d8526b2523
Randy Tolentino <makertolentino@gmail.com>
2016-09-10 19:54:02 -0500

    working on checkbox updating

---
+++ public/js/main.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+	var formCheckBox = document.getElementById('form-checkbox');
+	
+	formCheckBox.addEventListener('click', function(){
+		console.log('yo, you did it!')
+	})
+});
